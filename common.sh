e525acb6840f2adb6b723856ef94474eb98aa67b
Krzysztof Magosa <krzysztof@magosa.pl>
2019-08-27 09:46:39 +0200

    allow using it locally

--- common.sh
+++ common.sh
@@ -1,5 +1,7 @@
 set -e
 
+TRAVIS_COMMIT="${TRAVIS_COMMIT:-$(git rev-parse HEAD)}"
+
 version=$(grep ansible== ./requirements.txt | awk -F'==' '{print $2}')
 image="krzysztofmagosa/packer-ansible:${version}"
 
